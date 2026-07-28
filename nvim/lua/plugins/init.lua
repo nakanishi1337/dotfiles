d52473200185ab96043b8b7b03a6c827dae17990
nakanishi1337 <nakanishi1337@gmail.com>
2026-07-28 22:25:17 +0900

    nvim-tree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,4 +5,16 @@
 -- return {
 --   { "catppuccin/nvim", name = "catppuccin", opts = {} },
 -- }
-return {}
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+    },
+  },
+}
+
